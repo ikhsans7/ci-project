9e640a60acf267e302e570f8d5e6b104355569f5
ikhsans7 <abdulikhsanrozaki0708@gmail.com>
2021-10-25 12:55:21 +0700

    ubah text pada function idex

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -3,7 +3,7 @@ class Contoh1 extends CI_Controller
 {
     public function index()
     {
-        echo "<h1>Perkenalkan</h1>";
+        echo "<h1>Haloo Perkenalkan</h1>";
         echo"Nama saya Abdul Ikhsan 
             Saya tinggal di daerah Bumiayu
             Olahraga yang saya sukai adalah
